994cae46fcc8b5cec654d624ea851e954566f829
SavvyHex <paisaketh@gmail.com>
2024-06-28 22:47:30 +0530

    added a few functions for POC

---
+++ POC/utils.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+MAIN_URL = "https://vpic.nhtsa.dot.gov/api/"
+
+# Gets car model names based on the manufacturer
+def models_for_make(make:str="", modelyear:str="", vehicletype:str="") -> dict:
+    try:
+        if not make:
+            make = input("Enter the make for which you want the models of : ").lower().replace(" ", "%20")
+        url = MAIN_URL + f"/vehicles/GetModelsForMake/{make}{f"/modelyear/{modelyear}" if modelyear else ""}{f"/vehicletype/{vehicletype}" if vehicletype else ""}?format=json"
+        r = requests.get(url)
+        return json.loads(r.text)["Results"]
+    except Exception as e:
+        raise e
+
+# Gets information about the manufacturer
+def manufacturer_details(manufacturer:str="") -> dict:
+    try:
+        if not manufacturer:
+            manufacturer = input("Enter the manufacturer for which you want the details of : ").lower().replace(" ", "%20")
+        url = MAIN_URL + f"/vehicles/GetManufacturerDetails/{manufacturer}?format=json"
+        r = requests.get(url)
+        return json.loads(r.text)["Results"]
+    except Exception as e:
+        raise e
+
+# Returns the list of variables related to vehicles
+def vehicle_variables()-> dict:
+    return json.loads(requests.get(MAIN_URL+"/vehicles/GetVehicleVariableList?format=json").text)["Results"]
+
+# Returns possible values for each variable related to the vehicle
+def vehicle_variable_values(variable:str=""):
+     try:
+        if not variable:
+            variable = input("Enter the variable for which you want the possible values of : ").lower().replace(" ", "%20")
+        url = MAIN_URL + f"/vehicles/GetVehicleVariableValuesList/{variable}?format=json"
+        r = requests.get(url)
+        return json.loads(r.text)["Results"]
+    except Exception as e:
+        raise e
+ 
